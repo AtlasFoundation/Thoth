8247eb5299716ca02765ed51171290099b962256
Michael Sharpe <m.sharpe@project89.org>
2022-06-16 15:45:34 -0700

    Drop in plug wallet stuff

---
+++ packages/client/src/components/PlugWallet/PlugWallet.tsx
@@ -0,0 +1,110 @@
+import { useRef } from 'react'
+import './plugWallet.css'
+import { Mint } from '../Mint/Mint'
+
+export function PlugWallet({ onConnect, onFail }) {
+  // Code Goes Here
+
+  let userPrincipal = 'Not Connected'
+  let currentBalance = 'N/A'
+  let tokenName = ''
+  const dropdownModal = useRef<any>(null)
+  const balanceModal = useRef<any>(null)
+  const principalModal = useRef<any>(null)
+
+  const grabBalance = async () => {
+    balanceModal.current!.innerHTML = 'Please Wait...'
+    const res = await (window as any).ic.plug.requestBalance()
+    currentBalance = res[0].amount
+    tokenName = res[0].name
+    balanceModal.current!.innerHTML = currentBalance + ' ' + tokenName
+  }
+  const manageLogin = async () => {
+    await (window as any).ic.plug.requestConnect()
+    userPrincipal = await (window as any).ic.plug.agent.getPrincipal()
+    document.getElementById('statusBubble')!.style.backgroundColor =
+      'rgba(0,255,0,0.5)'
+    document.getElementById('connect')!.textContent = 'Connected!'
+    // @ts-ignore
+    document.getElementById('connect')!.disabled = true
+    console.log('Logged in as: ' + userPrincipal)
+    principalModal.current!.innerHTML = 'Logged In As: ' + userPrincipal
+    // activateDabFunctions();
+    await grabBalance()
+  }
+  const plugLogin = async () => {
+    // check if (window as any).ic exists, and if (window as any).ic.plug exist
+
+    if (!(window as any).ic || !(window as any).ic.plug) {
+      return onFail('Could not connect - Plug is not installed')
+    }
+
+    const hasLoggedIn = await (window as any).ic.plug.isConnected()
+    if (!hasLoggedIn) {
+      await manageLogin()
+    } else {
+      await (window as any).ic.plug.createAgent()
+      userPrincipal = await (window as any).ic.plug.agent.getPrincipal()
+
+      console.log('Logged in as: ' + userPrincipal)
+
+      if (!userPrincipal) {
+        return onFail('Could not connect - User authentication failedx')
+      }
+
+      document.getElementById('statusBubble')!.style.backgroundColor =
+        'rgba(0,255,0,0.5)'
+      document.getElementById('connect')!.textContent = 'Connected!'
+      // @ts-ignore
+      document.getElementById('connect')!.disabled = true
+      principalModal.current!.innerHTML = 'Logged In As: ' + userPrincipal
+      //   activateDabFunctions();
+      await grabBalance()
+      onConnect(userPrincipal)
+    }
+  }
+  const dropTheMenu = async () => {
+    dropdownModal.current!.classList.toggle('showMenu')
+  }
+  //   const activateDabFunctions = async() => {
+  //     DabStuff.methods?.activateDab();
+  //   }
+  window.onclick = function (event) {
+    event.preventDefault()
+    let dropdown = document.getElementById('plugSettings')!
+    if (!event.target!.matches('.plugMenu')) {
+      if (dropdown.classList.contains('showMenu')) {
+        dropdown.classList.remove('showMenu')
+      }
+    }
+  }
+  // HTML(UI) returns stay inside of the export function
+
+  return (
+    <>
+      <div className="walletContainer">
+        <button onClick={dropTheMenu} id="plugMenu" className="plugMenu">
+          Plug Menu<div className="statusBubble" id="statusBubble"></div>
+        </button>
+        <div className="plugSettings" id="plugSettings" ref={dropdownModal}>
+          <div className="menuHeader" id="menuHeader">
+            <button onClick={plugLogin} id="connect">
+              Connect
+            </button>
+            <h6 ref={principalModal}>Logged In As: {userPrincipal}</h6>
+            <div className="balance" id="balance">
+              <p>Balance: </p>
+              <p ref={balanceModal} style={{ color: 'rgba(0,255,0,0.5' }}>
+                {currentBalance} {tokenName}
+              </p>
+            </div>
+          </div>
+          <div className="menuDivider" />
+          <div className="menuBody">
+            <Mint />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
