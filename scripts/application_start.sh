dfb0d72a16f7a122accfe61a5e3b47f93fb6d33c
Kinjal <kinjalraval18591@gmail.com>
2022-07-10 15:42:19 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -10,15 +10,15 @@ cd /opt/thoth
 #install node modules
 yarn install
 
-cp /etc/letsencrypt/live/thoth.n3xus.city/privkey.pem /opt/thoth/client/certs/key.pem
-cp /etc/letsencrypt/live/thoth.n3xus.city/cert.pem /opt/thoth/client/certs/cert.pem
+cp /etc/letsencrypt/live/thoth.n3xus.city/privkey.pem /opt/thoth/packages/client/certs/key.pem
+cp /etc/letsencrypt/live/thoth.n3xus.city/cert.pem /opt/thoth/packages/client/certs/cert.pem
 
-cp /etc/letsencrypt/live/thoth.n3xus.city/privkey.pem /opt/thoth/server/certs/key.pem
-cp /etc/letsencrypt/live/thoth.n3xus.city/cert.pem /opt/thoth/server/certs/cert.pem
+cp /etc/letsencrypt/live/thoth.n3xus.city/privkey.pem /opt/thoth/packages/server/certs/key.pem
+cp /etc/letsencrypt/live/thoth.n3xus.city/cert.pem /opt/thoth/packages/server/certs/cert.pem
 
-cp /opt/thoth_env/server/.env /opt/thoth/server/.env
-cp /opt/thoth_env/client/.env /opt/thoth/client/.env
-cp /opt/thoth_env/core/.env /opt/thoth/core/.env
+cp /opt/thoth_env/server/.env /opt/thoth/packages/server/.env
+cp /opt/thoth_env/client/.env /opt/thoth/packages/client/.env
+cp /opt/thoth_env/core/.env /opt/thoth/packages/core/.env
 
 #start our node app in the background using PM2
 #pm2 --name thoth start "yarn run dev"
