04d2c02c03df9fe969528700c5a9316c03d3ad2a
Michael Sharpe <m.sharpe@project89.org>
2022-12-13 16:36:37 -0800

    Upgrade to react 18

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,6 +1,7 @@
 import './wdyr'
 import 'regenerator-runtime/runtime'
-import { render } from 'react-dom'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 // import { PersistGate } from 'redux-persist/integration/react'
@@ -12,7 +13,15 @@ import { store } from './state/store'
 
 const container = document.getElementById('root')
 
-render(
+// After
+// import { createRoot } from 'react-dom/client';
+//
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="home" />);
+
+const root = createRoot(container!) // createRoot(container!) if you use TypeScript
+
+const Root = () => (
   <Router>
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
@@ -21,9 +30,10 @@ render(
       </AppProviders>
       {/* </PersistGate> */}
     </Provider>
-  </Router>,
-  container
+  </Router>
 )
+
+root.render(<Root />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
